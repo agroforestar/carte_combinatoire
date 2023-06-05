0bb958702d813c7f3b745f845ddb954d612dbbae
Laetitia LEM <laetitia.lemiere@gmail.com>
2023-06-05 16:15:20 +0200

    Delete Crop.py

--- Crop.py
+++
@@ -1,8 +0,0 @@
-from Plant import Plant
-
-class Crop(Plant):
-    def __init__(self, specie, Xcoord, Ycoord):
-        super().__init__(specie, Xcoord, Ycoord)
-
-    def __repr__(self):
-        return repr((self.X, self.Y))
